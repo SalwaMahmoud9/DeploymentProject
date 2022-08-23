0528538f71d368787d99130110b99f5428169d77
SalwaMahmoud9 <108579776+SalwaMahmoud9@users.noreply.github.com>
2022-08-23 13:40:06 +0200

    Update environment.prod.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "udagram_app",
+  apiHost: "Udagramapp-env.eba-c5z83wi2.us-east-1.elasticbeanstalk.com ",
 };
 
 /*
