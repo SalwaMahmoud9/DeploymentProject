f47371b178557231ff93dd14e7f31abd13e3bc60
SalwaMahmoud9 <108579776+SalwaMahmoud9@users.noreply.github.com>
2022-08-25 15:02:52 +0200

    Update environment.ts

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { config } from "./config/config";
+import { config } from "../../../udagram-api/src/config/config";
 
 export const environment = {
   production: false,
